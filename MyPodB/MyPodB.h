ea551f9cf38971a6babcd42e1df60e001489bb24
Sergii Tatsii <sergiitatsii@gmail.com>
2018-12-13 19:53:15 +0300

    Initial Commit

---
+++ MyPodB/MyPodB.h
@@ -0,0 +1,19 @@
+//
+//  MyPodB.h
+//  MyPodB
+//
+//  Created by Sergii Tatsii on 12/13/18.
+//  Copyright © 2018 Sergii Tatsii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyPodB.
+FOUNDATION_EXPORT double MyPodBVersionNumber;
+
+//! Project version string for MyPodB.
+FOUNDATION_EXPORT const unsigned char MyPodBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPodB/PublicHeader.h>
+
+
